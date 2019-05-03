b4ce56b6d189db643386a029d51c3638e0a5fc54
Gene Rush <erush91@gmail.com>
2019-05-03 14:33:03 -0600

    added stop sign detector

--- image_proc/src/nodelets/scan.cpp
+++ image_proc/src/nodelets/scan.cpp
@@ -59,7 +59,7 @@ class ScanNodelet : public nodelet::Nodelet
   std::string target_frame_id_;
 
   boost::mutex connect_mutex_;
-  image_transport::CameraPublisher pub_;
+  image_transport::CameraPublisher pub_h_scans_;
   image_transport::CameraPublisher pub_h_scan_;
 
   // Dynamic reconfigure
@@ -97,12 +97,12 @@ void ScanNodelet::onInit()
   ReconfigureServer::CallbackType f = boost::bind(&ScanNodelet::configCb, this, _1, _2);
   reconfigure_server_->setCallback(f);
 
-  // Monitor whether anyone is subscribed to the output
+  // Monitor whether anyone is subscribed to the h_scans
   image_transport::SubscriberStatusCallback connect_cb = boost::bind(&ScanNodelet::connectCb, this);
   ros::SubscriberStatusCallback connect_cb_info = boost::bind(&ScanNodelet::connectCb, this);
-  // Make sure we don't enter connectCb() between advertising and assigning to pub_
+  // Make sure we don't enter connectCb() between advertising and assigning to pub_h_scans_
   boost::lock_guard<boost::mutex> lock(connect_mutex_);
-  pub_ = it_out_->advertiseCamera("h_scans",  1, connect_cb, connect_cb, connect_cb_info, connect_cb_info);
+  pub_h_scans_ = it_out_->advertiseCamera("h_scans",  1, connect_cb, connect_cb, connect_cb_info, connect_cb_info);
   pub_h_scan_ = it_out_->advertiseCamera("h_scan",  1, connect_cb, connect_cb, connect_cb_info, connect_cb_info);
   
 }
@@ -111,65 +111,12 @@ void ScanNodelet::onInit()
 void ScanNodelet::connectCb()
 {
   boost::lock_guard<boost::mutex> lock(connect_mutex_);
-  if (pub_.getNumSubscribers() == 0)
+  if (pub_h_scans_.getNumSubscribers() == 0 && pub_h_scan_.getNumSubscribers() == 0)
     sub_.shutdown();
   else if (!sub_)
   {
     image_transport::TransportHints hints("raw", ros::TransportHints(), getPrivateNodeHandle());
     sub_ = it_in_->subscribeCamera("image_raw", queue_size_, &ScanNodelet::imageCb, this, hints);
-  }
-}
-
-template <typename T>
-void debayer2x2toBGR(const cv::Mat& src, cv::Mat& dst, int R, int G1, int G2, int B)
-{
-  typedef cv::Vec<T, 3> DstPixel; // 8- or 16-bit BGR
-  dst.create(src.rows / 2, src.cols / 2, cv::DataType<DstPixel>::type);
-
-  int src_row_step = src.step1();
-  int dst_row_step = dst.step1();
-  const T* src_row = src.ptr<T>();
-  T* dst_row = dst.ptr<T>();
-
-  // 2x2 downsample and debayer at once
-  for (int y = 0; y < dst.rows; ++y)
-  {
-    for (int x = 0; x < dst.cols; ++x)
-    {
-      dst_row[x*3 + 0] = src_row[x*2 + B];
-      dst_row[x*3 + 1] = (src_row[x*2 + G1] + src_row[x*2 + G2]) / 2;
-      dst_row[x*3 + 2] = src_row[x*2 + R];
-    }
-    src_row += src_row_step * 2;
-    dst_row += dst_row_step;
-  }
-}
-
-// Templated on pixel size, in bytes (MONO8 = 1, BGR8 = 3, RGBA16 = 8, ...)
-template <int N>
-void decimate(const cv::Mat& src, cv::Mat& dst, int decimation_x, int decimation_y)
-{
-  dst.create(src.rows / decimation_y, src.cols / decimation_x, src.type());
-
-  int src_row_step = src.step[0] * decimation_y;
-  int src_pixel_step = N * decimation_x;
-  int dst_row_step = dst.step[0];
-
-  const uint8_t* src_row = src.ptr();
-  uint8_t* dst_row = dst.ptr();
-  
-  for (int y = 0; y < dst.rows; ++y)
-  {
-    const uint8_t* src_pixel = src_row;
-    uint8_t* dst_pixel = dst_row;
-    for (int x = 0; x < dst.cols; ++x)
-    {
-      memcpy(dst_pixel, src_pixel, N); // Should inline with small, fixed N
-      src_pixel += src_pixel_step;
-      dst_pixel += N;
-    }
-    src_row += src_row_step;
-    dst_row += dst_row_step;
   }
 }
 
@@ -187,9 +134,6 @@ void ScanNodelet::imageCb(const sensor_msgs::ImageConstPtr& image_msg,
   int decimation_x = config.decimation_x;
   int decimation_y = config.decimation_y;
 
-  // Compute the ROI we'll actually use
-  bool is_bayer = sensor_msgs::image_encodings::isBayer(image_msg->encoding);
-
   int max_width = image_msg->width - config.x_offset;
   int max_height = image_msg->height - config.y_offset;
   int width = config.width;
@@ -202,51 +146,55 @@ void ScanNodelet::imageCb(const sensor_msgs::ImageConstPtr& image_msg,
   // Get a cv::Mat view of the source data
   CvImageConstPtr source = toCvShare(image_msg);
 
-  // Except in Bayer downsampling case, output has same encoding as the input
-  CvImage output(source->header, source->encoding);
-  // Apply ROI (no copy, still a view of the image_msg data)
-  output.image = source->image(cv::Rect(config.x_offset, config.y_offset, width, height));
+  CvImage h_scans(source->header, source->encoding);
+  CvImage h_scans_float(source->header, source->encoding);
+  CvImage h_scan(source->header, "32FC1");
+
+  h_scans.image = source->image(cv::Rect(config.x_offset, config.y_offset, width, height));
+  h_scans_float.image = source->image(cv::Rect(config.x_offset, config.y_offset, width, height));
+  h_scan.image = source->image(cv::Rect(config.x_offset, config.y_offset, width, 1));
 
   ////////////////////////////////////
   // CALCULATE HORIZONTAL LINE SCAN //
   ////////////////////////////////////
 
-  // Average row of data and convert to meters
-  CvImage output_f(source->header, source->encoding);
-  CvImage h_scan(source->header, "32FC1");
-  output_f.image = source->image(cv::Rect(config.x_offset, config.y_offset, width, height));
-  h_scan.image = source->image(cv::Rect(config.x_offset, config.y_offset, width, 1));
-  output.image.convertTo(output_f.image, CV_32FC1,0.001); // [mm] --> [m]
-  cv::Mat row_mean, col_mean;
-  reduce(output_f.image, h_scan.image, 0, CV_REDUCE_AVG, CV_32FC1); // 0 = AVERAGE COLUMNS
-  std::cout << h_scan.image << std::endl;
+  // Convert to from 2 bytes (16 bit) to float, and scale from mm to m
+  h_scans.image.convertTo(h_scans_float.image, CV_32FC1,0.001); // [mm] --> [m]
+  
+  cv::Mat row_mean;
+  cv::Mat col_mean;
+  reduce(h_scans_float.image, h_scan.image, 0, CV_REDUCE_AVG, CV_32FC1); // 0 = AVERAGE COLUMNS
 
-  // Create output Image message
-  /// @todo Could save copies by allocating this above and having output.image alias it
-  sensor_msgs::ImagePtr out_image = output.toImageMsg();
+  // std::cout << h_scan.image << std::endl;
+
+  // Create h_scans Image message
+  sensor_msgs::ImagePtr out_h_scans = h_scans.toImageMsg();
   sensor_msgs::ImagePtr out_h_scan = h_scan.toImageMsg();
 
   // Create updated CameraInfo message
   sensor_msgs::CameraInfoPtr out_info = boost::make_shared<sensor_msgs::CameraInfo>(*info_msg);
   int binning_x = std::max((int)info_msg->binning_x, 1);
   int binning_y = std::max((int)info_msg->binning_y, 1);
-  out_info->binning_x = binning_x * config.decimation_x;
-  out_info->binning_y = binning_y * config.decimation_y;
+  out_info->binning_x = binning_x;
+  out_info->binning_y = binning_y;
   out_info->roi.x_offset += config.x_offset * binning_x;
   out_info->roi.y_offset += config.y_offset * binning_y;
   out_info->roi.height = height * binning_y;
   out_info->roi.width = width * binning_x;
+  
   // If no ROI specified, leave do_rectify as-is. If ROI specified, set do_rectify = true.
   if (width != (int)image_msg->width || height != (int)image_msg->height)
     out_info->roi.do_rectify = true;
 
   if (!target_frame_id_.empty()) {
-    out_image->header.frame_id = target_frame_id_;
+    out_h_scans->header.frame_id = target_frame_id_;
     out_info->header.frame_id = target_frame_id_;
   }
 
-  pub_.publish(out_image, out_info);
+  pub_h_scans_.publish(out_h_scans, out_info);
   pub_h_scan_.publish(out_h_scan, out_info);
+
+  
 }
 
 void ScanNodelet::configCb(Config &config, uint32_t level)
